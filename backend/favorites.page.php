9e0f54fe6e0a76f78e2e4f23c926f5f6cee36ebe
dami2004 <d.emelianoff@gmail.com>
2023-12-17 01:13:39 +0300

    изменила формирование массива

--- backend/favorites.page.php
+++ backend/favorites.page.php
@@ -1,25 +1,42 @@
 <?php
 include_once "db.php";
 
-$postData = file_get_contents("php://input");
+// $postData = file_get_contents("php://input");
 
-$data = json_decode($postData, true);
+// $data = json_decode($postData, true);
 
-if ($data === null || !isset($data['userId'])) {
-    $response = array('status' => 'error', 'message' => 'Ошибка при декодировании JSON или отсутствие необходимых данных');
-} else {
-    $userId = $data['userId'];
+// if ($data === null || !isset($data['userId'])) {
+//     $response = array('status' => 'error', 'message' => 'Ошибка при декодировании JSON или отсутствие необходимых данных');
+// } else {
+//     $userId = $data['userId'];
 
-    $stmt = $db_connect->prepare('SELECT userId, url, rating FROM favorites join users on users.id=userId WHERE userId=:post_user_id;');
-    $stmt->bindValue(':post_user_id',$userId);
-    $stmt->execute();
-    $mas=[];
-    while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
-            $mas[]=$result;
-    }
-    $response = array('status' => 'success', 'body'=>$mas);
+//     $stmt = $db_connect->prepare('SELECT userId, url, rating FROM favorites join users on users.id=userId WHERE userId=:post_user_id;');
+//     $stmt->bindValue(':post_user_id',$userId);
+//     $stmt->execute();
+//     $mas=[];
+//     while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
+//             $mas[]=$result;
+//     }
+//     $response = array('status' => 'success', 'body'=>$mas);
+
+// }
+// header('Content-Type: application/json');
+// echo json_encode($response);
+
+$userId = 1;
+
+$stmt = $db_connect->prepare('SELECT userId, url, rating FROM favorites join users on users.id=userId WHERE userId=:post_user_id;');
+$stmt->bindValue(':post_user_id',$userId);
+$stmt->execute();
+$mas=[];
+$response = array('status' => 'success');
+while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
+    // print_r($result);
+    $response[] = ['userid'=>$result['userid'],
+    'url'=>$result['url'],
+    'rating'=>$result['rating'],
+];
 
 }
-header('Content-Type: application/json');
 echo json_encode($response);
 ?>
