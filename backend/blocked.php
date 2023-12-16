3538c82deb26228bb63ebe7a1f3063ff0be4c1f1
dami2004 <d.emelianoff@gmail.com>
2023-12-17 01:20:36 +0300

    исправила функции

--- backend/blocked.php
+++ backend/blocked.php
@@ -1,18 +1,24 @@
 <?php
 include_once "db.php";
 
-$userId = 1;
+$postData = file_get_contents("php://input");
 
-$stmt = $db_connect->prepare('SELECT userId, url FROM blocked join users on users.id=userId WHERE userId=:post_user_id;');
-$stmt->bindValue(':post_user_id',$userId);
-$stmt->execute();
-$mas=[];
-$response = array('status' => 'success');
-while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
-    $response[] = ['userid'=>$result['userid'],
-    'url'=>$result['url'],
-];
+$data = json_decode($postData, true);
 
+if ($data === null || !isset($data['userId'])) {
+    $response = array('status' => 'error', 'message' => 'Ошибка при декодировании JSON или отсутствие необходимых данных');
+} else {
+    $userId = $data['userId'];
+    $stmt = $db_connect->prepare('SELECT userId, url FROM blocked join users on users.id=userId WHERE userId=:post_user_id;');
+    $stmt->bindValue(':post_user_id',$userId);
+    $stmt->execute();
+    $response = array('status' => 'success');
+    while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
+        $response[] = ['userid'=>$result['userid'],
+        'url'=>$result['url'],
+        ];
+    }
 }
+header('Content-Type: application/json');
 echo json_encode($response);
 ?>
--- backend/favorites.page.php
+++ backend/favorites.page.php
@@ -1,17 +1,26 @@
 <?php
 include_once "db.php";
 
-$stmt = $db_connect->prepare('SELECT userId, url, rating FROM favorites join users on users.id=userId WHERE userId=:post_user_id;');
-$stmt->bindValue(':post_user_id',$userId);
-$stmt->execute();
-$mas=[];
-$response = array('status' => 'success');
-while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
-    $response[] = ['userid'=>$result['userid'],
-    'url'=>$result['url'],
-    'rating'=>$result['rating'],
-];
+$postData = file_get_contents("php://input");
 
+$data = json_decode($postData, true);
+
+if ($data === null || !isset($data['userId'])) {
+    $response = array('status' => 'error', 'message' => 'Ошибка при декодировании JSON или отсутствие необходимых данных');
+} else {
+    $userId = $data['userId'];
+
+    $stmt = $db_connect->prepare('SELECT userId, url, rating FROM favorites join users on users.id=userId WHERE userId=:post_user_id;');
+    $stmt->bindValue(':post_user_id',$userId);
+    $stmt->execute();
+    $response = array('status' => 'success');
+    while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
+        $response['body'] = ['userid'=>$result['userId'],
+        'url'=>$result['url'],
+        'rating'=>$result['rating'],
+        ];
+    }
 }
+header('Content-Type: application/json');
 echo json_encode($response);
 ?>
