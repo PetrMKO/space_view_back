a490b04271043216156cc6eac062eac419c6dac1
dami2004 <d.emelianoff@gmail.com>
2023-12-17 01:15:37 +0300

    получение заблокированных фото

---
+++ backend/blocked.php
@@ -0,0 +1,19 @@
+<?php
+include_once "db.php";
+
+$userId = 1;
+
+$stmt = $db_connect->prepare('SELECT userId, url FROM blocked join users on users.id=userId WHERE userId=:post_user_id;');
+$stmt->bindValue(':post_user_id',$userId);
+$stmt->execute();
+$mas=[];
+$response = array('status' => 'success');
+while($result=$stmt->fetch(\PDO::FETCH_ASSOC)){
+    // print_r($result);
+    $response[] = ['userid'=>$result['userid'],
+    'url'=>$result['url'],
+];
+
+}
+echo json_encode($response);
+?>
